99f60db393ab1cd79b5e8fa2a74a944ba6d59ebf
Mikel García <mikel.gdaa@gmail.com>
2021-07-31 11:04:32 +0200

    Build

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["yeet-pythtb"],             # Name of the python package
+    py_modules=["yeet_pythtb"],             # Name of the python package
     package_dir={'':'yeet-pythtb/src'},     # Directory of the source code of the package
     install_requires=["numba","matplotlib","numpy"]                     # Install other dependencies if any
 )
